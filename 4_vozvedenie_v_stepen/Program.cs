14dc8cc4e990717b7c1b176da45f6fdc806dd0bd
AlexeyWatt <allexdoma2@gmail.com>
2022-05-20 12:24:08 +0300

    добавил задачу 4

---
+++ 4_vozvedenie_v_stepen/Program.cs
@@ -0,0 +1,8 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.WriteLine("Введите число A");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int a_res = a;
+Console.WriteLine("Введите число B");
+int b = int.Parse(Console.ReadLine() ?? "0");
+for (int i = 1; i < b; i++) a_res *= a;
+Console.Write($"Число {a} в степени {b} = {a_res}");
