268049fe7b267f9134f9ee00d611e02f102a72a6
AlexeyWatt <allexdoma2@gmail.com>
2022-05-20 11:57:58 +0300

    добавил задачу 1

---
+++ kvadrati_chisel/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Квадраты чисел от 1 до {n} :");
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"{i} -> {i*i}");
+}
