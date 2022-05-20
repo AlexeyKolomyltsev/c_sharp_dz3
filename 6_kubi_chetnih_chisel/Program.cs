2199be1ab428548b9497db62f28adec06475af3d
AlexeyWatt <allexdoma2@gmail.com>
2022-05-20 12:58:52 +0300

    добавил задачу 6

---
+++ 6_kubi_chetnih_chisel/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать кубы четных чисел от A до B
+Console.WriteLine("Введите число A");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число B");
+int b = int.Parse(Console.ReadLine() ?? "0");
+int aForRes = a;
+if (a % 2 != 0) a += 1;
+Console.WriteLine($"Кубы четных чисел от {aForRes} до {b} :");
+for (int i = a; i <= b; i += 2)
+{
+    Console.WriteLine($"{i} -> {i * i * i}");
+}
