169e2dd41ce47002accae44ff02469bb77ed132f
AlexeyWatt <allexdoma2@gmail.com>
2022-05-20 12:11:19 +0300

    добавил задачу 3

---
+++ 3_summa_chisel/Program.cs
@@ -0,0 +1,6 @@
+﻿// Найти сумму чисел от 1 до А
+Console.WriteLine("Введите число A");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int count = 1;
+for (int i = 2; i <= n; i++) count += i;
+Console.Write($"Сумма чисел от 1 до {n} : {count}");
