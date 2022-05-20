fdaf9bb804a3d72ddd5ef4ab29d18a7801c46b8d
AlexeyWatt <allexdoma2@gmail.com>
2022-05-20 12:30:18 +0300

    добавил задачу 5

---
+++ 5_summa_cifr_v_chisle/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число A");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int count = 0;
+int nRes = n;
+while (nRes != 0)
+{
+    count += nRes % 10;
+    nRes = nRes / 10;
+}
+Console.Write($"Сумма сумму цифр в числе {n} : {count}");
